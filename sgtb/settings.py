44f090793f854151697e8477132e43afad7f5c3a
Douglas Luna <53260309+DipionX02@users.noreply.github.com>
2020-06-03 00:47:09 -0300

    Update settings.py

--- sgtb/settings.py
+++ sgtb/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sgtb_db',
         'USER': 'postgres',
-        'PASSWORD': 'django@123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
